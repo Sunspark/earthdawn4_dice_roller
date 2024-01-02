8a5c1952aeb5f491ac5d95ba7762c0a16b112bdb
Will List <will@will-list.co.uk>
2024-01-02 11:48:56 +0000

    Difficulty removed

--- earthdawn_dice.js
+++ earthdawn_dice.js
@@ -18,18 +18,16 @@ function makeRoller()
 	var row1 = $.create("tr", table);
 	$.create("td", row1, {"innerHTML": "Step:"}, {"width": "15px"});
 	var stepInput = $.create("input", $.create("td", row1, false, {"width": "65px"}), {"type": "text"}, {"width": "100%"});
-	$.create("td", row1, {"innerHTML": "Difficulty:"}, {"width": "15px"});
-	var diffInput = $.create("input", $.create("td", row1, false, {"width": "65px"}), {"type": "text"}, {"width": "100%"});
 	$.create("td", row1, {"innerHTML": "Karma:"}, {"width": "45px"});
 	var karmaInput = $.create("input", $.create("td", row1, false, {"width": "10px"}), {"type": "checkbox"}, {"width": "10px"});
 	var roll = $.create("td", row1, {"innerHTML": "<input type=\"button\" value=\"Roll\"/>"});
 	var row2 = $.create("tr", table);
 	$.create("td", row2, {"innerHTML": "Label:"});
-	var labelInput = $.create("input", $.create("td", row2, {"colSpan": 6}), {"type": "text"}, {"width": "100%"});
-	roll.addEventListener("click", makeRoll(stepInput, diffInput, labelInput, karmaInput), false);
+	var labelInput = $.create("input", $.create("td", row2, {"colSpan": 4}), {"type": "text"}, {"width": "100%"});
+	roll.addEventListener("click", makeRoll(stepInput, labelInput, karmaInput), false);
 }
 
-function makeRoll(stepInput, diffInput, labelInput, karmaInput)
+function makeRoll(stepInput, labelInput, karmaInput)
 {
 	var outputArea = $.id("output");
 	return function()
@@ -37,11 +35,8 @@ function makeRoll(stepInput, diffInput, labelInput, karmaInput)
 		var step = parseInt(trim(stepInput.value));
 		if (!isNaN(step))
 		{
-			var diff = parseInt(trim(diffInput.value));
-			if (isNaN(diff))
-				diff = null;
 			var karma = karmaInput.checked;
-			var result = roll(step, karma, diff);
+			var result = roll(step, karma);
 			result.label = trim(labelInput.value);
 			result.step = step;
 			var paragraph = $.create("p", outputArea);
@@ -54,23 +49,7 @@ function makeRoll(stepInput, diffInput, labelInput, karmaInput)
 function output(result, paragraph)
 {
 	paragraph.innerHTML = resultsToString(result);
-	$.create("a", paragraph, {"innerHTML": "(change difficulty)", "className": "blue"}).addEventListener("click", makeChangeDifficulty(result, paragraph), false);
 	
-}
-
-function makeChangeDifficulty(result, paragraph)
-{
-	return function()
-	{
-		var newDifficulty = prompt("What is the new difficulty?", isNaN(result.difficulty) ? "" : result.difficulty);
-		newDifficulty = parseInt(newDifficulty);
-		if (newDifficulty)
-		{
-			result.difficulty = newDifficulty;
-			setResult(result);
-			output(result, paragraph);
-		}
-	}
 }
 
 function reset()
