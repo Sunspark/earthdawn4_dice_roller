0c3b56bf7dcb28bfc3b32c747e7f97e8140064d6
Will List <will@will-list.co.uk>
2024-01-02 17:06:47 +0000

    Dice rolling re-implemented to roll and explode individual dice (to allow dice to be mucked with individually).
    Provision (under the hood) added for multiple
    karma/ devotion at variable steps.

--- earthdawn_core.js
+++ earthdawn_core.js
@@ -1,75 +1,136 @@
-var stepChart = 
-[
-	[1],
-	[6, -3],
-	[6, -2],
-	[6, -1],
-	[6],
-	[8],
-	[10],
-	[12],
-	[6, 6],
-	[8, 6],
-	[8, 8],
-	[10, 8],
-	[10, 10],
-	[12, 10],
-	[12, 12]
-];
+var stepDict = {
+  // up to step 7
+  "basicSteps" : [
+    4 // step 1 is D4! then minus 2 (EDPG 4th p33)
+    , 4 // step 2 is D4! then minus 1 (EDPG 4th p33)
+    , 4
+    , 6
+    , 8
+    , 10
+    , 12
+  ]
+  // steps 8 - 18 repeat up the step chart, with an extra d20 each time.
+  , "repeatStepDict" : {
+    "8" : [6, 6]
+    , "9" : [6, 8]
+    , "10" : [8, 8]
+    , "11" : [10, 8]
+    , "12" : [10, 10]
+    , "13" : [12, 10]
+    , "14" : [12, 12]
+    , "15" : [12, 6, 6]
+    , "16" : [12, 8, 6]
+    , "17" : [12, 8, 8]
+    , "18" : [12, 10, 8]
+  }
+}
+
 
 function random(a, b)
 {
-	return Math.floor(Math.random()*(b-a+1)+a);
+  return Math.floor(Math.random()*(b-a+1)+a);
 }
 
-function roll(stepNum, karma)
+function recursiveRoll(die, rollArr)
 {
-	if (stepNum < 0)
-		stepNum = 0;
-	var results = {"rolls": new Array(), "successes": 0, "ones": 0, "result": -1, "step": stepNum};
-	var d12s = Math.max(0, Math.floor((stepNum-8)/7));
-	var dice = cloneArray(stepChart[stepNum-7*d12s]);
-	for (var i = 0; i < d12s; i++)
-		dice.push(12);
-	if (karma)
-		dice.push(6);
-	for (var i = 0; i < dice.length; i++)
-	{
-		if (dice[i] < 1)
-			results.successes += dice[i];
-		else
-		{
-			var roll = random(1, dice[i]);
-			results.rolls.push(roll);
-			results.successes += roll;
-			if (roll == 1)
-				results.ones++;
-			else if (roll == dice[i])
-				i--;
-		}
-	}
-	if (results.successes < 1)
-		results.successes = 1;
-		
-	return results;
+  var roll = random(1, die);
+  rollArr.push(roll);
+  if (roll == die) {
+    rollArr = recursiveRoll(die, rollArr);
+  }
+
+  return rollArr;
 }
 
-function cloneArray(arr)
+function getStepDice(stepNum)
 {
-	var clone = new Array();
-	for (var i = 0; i < arr.length; i++)
-		clone[i] = arr[i];
-	return clone;
+  var diceArr = [];
+  if (stepNum <= 7) {
+    // get from the basic steps
+    diceArr.push(stepDict['basicSteps'][stepNum-1]);
+  } else {
+    var extraD20s = (Math.floor(stepNum / 11) - 1);
+    if (extraD20s < 0) { extraD20s = 0; }
+    for (var i = 0; i < extraD20s; i++) {diceArr.push(20)};
+
+    var tempStepNum = stepNum - (extraD20s * 11);
+    diceArr = diceArr.concat(stepDict['repeatStepDict'][tempStepNum]);
+  }
+  return diceArr;
 }
 
-function resultsToString(results)
+function doStepDiceRoll(dice)
 {
-	var str = "";
-	if (results.label != "")
-		str += "<b>" + results.label + "</b><br/>";
-	str += results.rolls.join(", ") + "<br/><b>Total:</b> " + results.successes;
-	str += " on step " + results.step + " ";
-	if (results.ones == results.rolls.length)
-		str += " <b>AUTOMATIC FAILURE</b> ";
-	return str;
+  var stepResultDict = {
+    "rolls": new Array()
+    , "ones": 0
+  };
+  for (var i = 0; i < dice.length; i++) {
+    var rollArr = recursiveRoll(dice[i], []);
+    stepResultDict.rolls = stepResultDict.rolls.concat(rollArr);
+    if (rollArr[0] == 1) { stepResultDict.ones++; }
+  }
+  return stepResultDict;
+}
+
+function doStepRoll(stepNum)
+{
+  // figure out the dice for the step
+  dice = getStepDice(stepNum);
+
+  // do the step rolls
+  stepResultDict = doStepDiceRoll(dice);
+
+  // if step was 1 or 2, take modifier off.
+  if (stepNum == 1) {
+    stepResultDict.rolls.push(-2)
+  } else if (stepNum == 2) {
+    stepResultDict.rolls.push(-1)
+  }
+  return stepResultDict;
+}
+
+function AppendRollResults(results, stepNum)
+{
+  stepResultDict = doStepRoll(stepNum);
+  results.rolls = results.rolls.concat(stepResultDict.rolls);
+  results.ones += stepResultDict.ones;
+  return results;
+}
+
+function roll(coreStepNum, karmaNumber, karmaStepNum, devotionNumber, devotionStepNum)
+{
+  var results = {
+    "rolls": new Array()
+    , "rollTotal": 0
+    , "ones": 0
+    , "result": -1
+    , "step": ""
+  };
+  
+  // roll the core step dice
+  if (coreStepNum > 0) {
+    results = AppendRollResults(results, coreStepNum);
+    results.step = results.step.concat(coreStepNum);
+  }
+
+  // roll the dice for Karma
+  if (karmaNumber > 0 && karmaStepNum > 0) {
+    results = AppendRollResults(results, karmaStepNum);
+    results.step = results.step.concat(" plus 1 Karma at step " + karmaStepNum);
+  }
+
+  // roll the dice for Devotion
+  if (devotionNumber > 0 && devotionStepNum > 0) {
+    results = AppendRollResults(results, devotionStepNum);
+    results.step = results.step.concat(" plus X Devotion at step " + devotionStepNum);
+  }
+
+  // total the results
+  for (var i = 0; i < results.rolls.length; i++) {
+    results.rollTotal += results.rolls[i];
+  }
+  if (results.rollTotal < 1) {results.rollTotal = 1;}
+    
+  return results;
 }
--- earthdawn_dice.js
+++ earthdawn_dice.js
@@ -1,66 +1,89 @@
 function trim(str)
 {
-	return str.replace(/^\s+|\s+$/g,"");
+  return str.replace(/^\s+|\s+$/g,"");
 }
 
 function setup()
 {
-	$.create("input", $.id("reset"), {"type": "button", "value": "Clear Output"}).addEventListener("click", clearOutput, false);
-	$.create("input", $.id("reset"), {"type": "button", "value": "+"}, {"marginLeft": "10px"}).addEventListener("click", makeRoller, false);
-	//$.create("input", $.id("reset"), {"type": "button", "value": "Reset"}, {"marginLeft": "10px"}).addEventListener("click", reset, false);
-	for (var i = 0; i < 6; i++)
-		makeRoller();
+  $.create("input", $.id("reset"), {"type": "button", "value": "Clear Output"}).addEventListener("click", clearOutput, false);
+  $.create("input", $.id("reset"), {"type": "button", "value": "+"}, {"marginLeft": "10px"}).addEventListener("click", makeRoller, false);
+  //$.create("input", $.id("reset"), {"type": "button", "value": "Reset"}, {"marginLeft": "10px"}).addEventListener("click", reset, false);
+  for (var i = 0; i < 6; i++)
+    makeRoller();
 }
 
 function makeRoller()
 {
-	var table = $.create("table", $.id("rollers"), false, {"width": "100%", "marginBottom": "20px"});
-	var row1 = $.create("tr", table);
-	$.create("td", row1, {"innerHTML": "Step:"}, {"width": "15px"});
-	var stepInput = $.create("input", $.create("td", row1, false, {"width": "65px"}), {"type": "text"}, {"width": "100%"});
-	$.create("td", row1, {"innerHTML": "Karma:"}, {"width": "45px"});
-	var karmaInput = $.create("input", $.create("td", row1, false, {"width": "10px"}), {"type": "checkbox"}, {"width": "10px"});
-	var roll = $.create("td", row1, {"innerHTML": "<input type=\"button\" value=\"Roll\"/>"});
-	var row2 = $.create("tr", table);
-	$.create("td", row2, {"innerHTML": "Label:"});
-	var labelInput = $.create("input", $.create("td", row2, {"colSpan": 4}), {"type": "text"}, {"width": "100%"});
-	roll.addEventListener("click", makeRoll(stepInput, labelInput, karmaInput), false);
+  var table = $.create("table", $.id("rollers"), false, {"width": "100%", "marginBottom": "20px"});
+  var row1 = $.create("tr", table);
+  $.create("td", row1, {"innerHTML": "Step:"}, {"width": "15px"});
+  var stepInput = $.create("input", $.create("td", row1, false, {"width": "65px"}), {"type": "text"}, {"width": "100%"});
+  $.create("td", row1, {"innerHTML": "Karma:"}, {"width": "45px"});
+  var karmaInput = $.create("input", $.create("td", row1, false, {"width": "10px"}), {"type": "checkbox"}, {"width": "10px"});
+  var roll = $.create("td", row1, {"innerHTML": "<input type=\"button\" value=\"Roll\"/>"});
+  var row2 = $.create("tr", table);
+  $.create("td", row2, {"innerHTML": "Label:"});
+  var labelInput = $.create("input", $.create("td", row2, {"colSpan": 4}), {"type": "text"}, {"width": "100%"});
+  roll.addEventListener("click", makeRoll(stepInput, labelInput, karmaInput), false);
 }
 
 function makeRoll(stepInput, labelInput, karmaInput)
 {
-	var outputArea = $.id("output");
-	return function()
-	{
-		var step = parseInt(trim(stepInput.value));
-		if (!isNaN(step))
-		{
-			var karma = karmaInput.checked;
-			var result = roll(step, karma);
-			result.label = trim(labelInput.value);
-			result.step = step;
-			var paragraph = $.create("p", outputArea);
-			output(result, paragraph);
-			outputArea.scrollTop = outputArea.scrollHeight;
-		}
-	}
+  var outputArea = $.id("output");
+  return function()
+  {
+    var stepNum = parseInt(trim(stepInput.value));
+    if (!isNaN(stepNum))
+    {
+
+			// figure out Karma
+			var karmaNumber = 0;
+			var karmaStepNum = 6;
+      var karma = karmaInput.checked;
+			if(karma){karmaNumber=1;}
+
+			// figure out Devotion
+			var devotionNumber = 0;
+			var devotionStepNum = 6;
+
+      var result = roll(stepNum, karmaNumber, karmaStepNum, devotionNumber, devotionStepNum);
+      result.label = trim(labelInput.value);
+      var paragraph = $.create("p", outputArea);
+      output(result, paragraph);
+      outputArea.scrollTop = outputArea.scrollHeight;
+    }
+  }
+}
+
+function resultsToString(results)
+{
+  var str = "";
+  if (results.label != "") {
+    str += "<b>" + results.label + "</b><br/>";
+  }
+  str += results.rolls.join(", ") + "<br/><b>Total:</b> " + results.rollTotal;
+  str += " on Step ";
+  str += results.step;
+  if (results.ones == results.rolls.length)
+    str += " <b>AUTOMATIC FAILURE</b> ";
+  return str;
 }
 
 function output(result, paragraph)
 {
-	paragraph.innerHTML = resultsToString(result);
-	
+  paragraph.innerHTML = resultsToString(result);
+  
 }
 
 function reset()
 {
-	$.id("output").innerHTML = "";
-	$.id("rollers").innerHTML = "";
-	$.id("reset").innerHTML = "";
-	setup();
+  $.id("output").innerHTML = "";
+  $.id("rollers").innerHTML = "";
+  $.id("reset").innerHTML = "";
+  setup();
 }
 
 function clearOutput()
 {
-	$.id("output").innerHTML = "";
+  $.id("output").innerHTML = "";
 }
